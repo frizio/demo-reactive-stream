f7286cd3af5053ca0164eeeca5dd42e3de3cdc5a
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-07-29 11:27:27 +0200

    Add main code

--- src/main/java/cloud/frizio/demo/App.java
+++ src/main/java/cloud/frizio/demo/App.java
@@ -7,7 +7,16 @@ package cloud.frizio.demo;
 public class App 
 {
     public static void main( String[] args ) {
+
         System.out.println( "Hello World!" );
         System.out.println("java.version " + System.getProperty("java.version"));
+
+        SimplePublisher publisher = new SimplePublisher(5);
+        SimpleProcessor processor = new SimpleProcessor();
+        SimpleSubscriber subscriber = new SimpleSubscriber();
+            
+        publisher.subscribe( processor );	
+        processor.subscribe( subscriber );
+
     }
 }

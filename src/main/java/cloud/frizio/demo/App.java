e7bf136eedb1db9e12767c21c2518de14ddc014f
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-07-29 11:08:29 +0200

    first commit

---
+++ src/main/java/cloud/frizio/demo/App.java
@@ -0,0 +1,13 @@
+package cloud.frizio.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        System.out.println("java.version " + System.getProperty("java.version"));
+    }
+}
